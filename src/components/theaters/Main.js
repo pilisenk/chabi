7c9c886af326fea929f846e1c49bab07e9871abb
pilisenk <pilisenk@gmail.com>
2020-03-21 23:31:57 +0800

    upgrade all packages add counter

--- src/components/theaters/Main.js
+++ src/components/theaters/Main.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import Mall from '../legions/Mall'
 import Cart from '../legions/Cart'
 
 const Main = props => {
+  const [counter, setCounter] = useState(0)
+
+  useEffect(() => {
+    const interval = setInterval(() => setCounter(c => c + 1), 1000)
+    return () => clearInterval(interval)
+  }, [])
+
   return (
     <main className="main">
+      <div className="gg">{counter}</div>
       <Mall />
       <Cart />
     </main>
